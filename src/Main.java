86a18bfa8cfc22bacc97f126220daaef39c20dfa
tailoryash <theyash.tailor@gmail.com>
2023-04-05 10:51:54 +0530

    Main.java: updated few changes

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         Employee e = new Employee();
-        String name = scn.nextLine();
-        int id = scn.nextInt();
 
-        String dept = scn.next();
-        e.setEmpid(id);
+        System.out.print("Enter Employee name : ");
+        String name = scn.nextLine();
         e.setName(name);
+
+        System.out.print("Enter Employee Id : ");
+        int id = scn.nextInt();
+        e.setEmpid(id);
+
+        System.out.print("Enter Employee Dept : ");
+        String dept = scn.next();
         e.setDept(dept);
-        System.out.println("Welcome to Simform !");
+
+        System.out.println("Welcome to Simform Family !!");
         System.out.println("Employee ID : " + e.getEmpid());
         System.out.println("Employee name : " + e.getName());
         System.out.println("Employee Dept : " + e.getDept());
