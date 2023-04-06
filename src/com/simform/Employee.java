fb5500c4f98259b245c85ac2ceba995ea5f655b3
tailoryash <theyash.tailor@gmail.com>
2023-04-06 11:35:40 +0530

    added packages and fix naming conventions

---
+++ src/com/simform/Employee.java
@@ -0,0 +1,31 @@
+package com.simform;
+
+public class Employee {
+    private int empId;
+    private String empName;
+    private String empDept;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpDept() {
+        return empDept;
+    }
+
+    public void setEmpDept(String empDept) {
+        this.empDept = empDept;
+    }
+}
--- src/com/simform/Main.java
+++ src/com/simform/Main.java
@@ -1,28 +1,31 @@
+package com.simform;
+
 import java.util.Scanner;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        Employee e = new Employee();
+        Employee emp = new Employee();
 
         System.out.print("Enter Employee name : ");
         String name = scn.nextLine();
-        e.setName(name);
+        emp.setEmpName(name);
 
         System.out.print("Enter Employee Id : ");
         int id = scn.nextInt();
-        e.setEmpid(id);
+        emp.setEmpId(id);
 
         System.out.print("Enter Employee Dept : ");
         String dept = scn.next();
-        e.setDept(dept);
+        emp.setEmpDept(dept);
 
         System.out.println("Welcome to Simform Family !!");
-        System.out.println("Employee ID : " + e.getEmpid());
-        System.out.println("Employee name : " + e.getName());
-        System.out.println("Employee Dept : " + e.getDept());
+        System.out.println("Employee ID : " + emp.getEmpDept());
+        System.out.println("Employee name : " + emp.getEmpName());
+        System.out.println("Employee Dept : " + emp.getEmpDept());
 
     }
 }
